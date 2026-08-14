14b07c1f42b2bc86625f7806059f3ddd76dfb917
Wout Van Wijmeersch <125986892+woutvanwijmeersch@users.noreply.github.com>
2026-08-14 16:46:29 +0200

    Missing content

--- client/src/Components/Courses/CourseDetail.js
+++ client/src/Components/Courses/CourseDetail.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown';
-import Context from '../../Context';
-import Loading from '../Loading';
+import React, { useState, useEffect, useContext } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import Context from "../../Context";
+import Loading from "../Loading";
 
 const CourseDetail = () => {
   const context = useContext(Context.Context);
-  let courseDetail = useState('');
+  let courseDetail = useState("");
   const [course, setCourseDetail] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const authUser = context.authenticatedUser;
@@ -17,18 +17,19 @@ const CourseDetail = () => {
   useEffect(() => {
     // Fetch a course from the database
     const controller = new AbortController();
-    context.data.getCourse(id)
-      .then(response => {
+    context.data
+      .getCourse(id)
+      .then((response) => {
         if (response.id) {
-          setCourseDetail(response)
+          setCourseDetail(response);
         } else {
           // If there is no course ID, direct to Not Found
-          navigate('/notfound');
+          navigate("/notfound");
         }
       })
       .catch((error) => {
-        console.error('Error fetching and parsing course', error);
-        navigate('/error');
+        console.error("Error fetching and parsing course", error);
+        navigate("/error");
       })
       .finally(() => {
         setIsLoading(false);
@@ -38,69 +39,71 @@ const CourseDetail = () => {
   }, [id, navigate, context.data]);
 
   if (course.id) {
-    courseDetail = <div className="wrap">
-      <h2>Course Detail</h2>
-      <div className="main--flex">
-        <div>
-          <h3 className="course--detail--title">Course</h3>
-          <h4 className="course--name">{course.title}</h4>
-          {course.User
-            ? (<p>By {course.User.firstName} {course.User.lastName}</p>)
-            : null
-          }
-          <ReactMarkdown>{course.description}</ReactMarkdown>
-        </div>
-        <div>
-          <h3 className="course--detail--title">Estimated Time</h3>
-          <p>{course.estimatedTime}</p>
-
-          <h3 className="course--detail--title">Materials Needed</h3>
-          <ul className="course--detail--list">
-            <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
-          </ul>
+    courseDetail = (
+      <div className="wrap">
+        <h2>Course Detail</h2>
+        <div className="main--flex">
+          <div>
+            <h3 className="course--detail--title">Course</h3>
+            <h4 className="course--name">{course.title}</h4>
+            {course.User ? (
+              <p>
+                By {course.User.firstName} {course.User.lastName}
+              </p>
+            ) : null}
+            <ReactMarkdown>{course.description}</ReactMarkdown>
+          </div>
+          <div>
+            <h3 className="course--detail--title">Estimated Time</h3>
+            <p>{course.estimatedTime}</p>
+          </div>
         </div>
       </div>
-    </div>
+    );
   }
 
   const handleDelete = (event) => {
     event.preventDefault();
-    context.data.deleteCourse(id, authUser.emailAddress, authUser.password)
+    context.data
+      .deleteCourse(id, authUser.emailAddress, authUser.password)
       .then((response) => {
         // If course deletion is successful, then there should be no response returned
         if (response.length) {
-          navigate('/error');
+          navigate("/error");
         } else {
-          navigate('/');
+          navigate("/");
         }
       })
       .catch((error) => {
         console.error(error);
-        navigate('/error');
+        navigate("/error");
       });
-  }
+  };
 
-  return (
-    isLoading ?
-      <Loading />
-      : course ? <div>
-        <div className="actions--bar">
-          <div className="wrap">
-            {authUser && (authUser.id === course.User.id) ?
-              <Link to={`/courses/${id}/update`} className="button">Update Course</Link>
-              : null
-            }
-            {authUser && (authUser.id === course.User.id) ?
-              <button className="button" onClick={handleDelete}>Delete Course</button>
-              : null
-            }
-            <Link to='/' className="button button-secondary">Return to List</Link>
-          </div>
+  return isLoading ? (
+    <Loading />
+  ) : course ? (
+    <div>
+      <div className="actions--bar">
+        <div className="wrap">
+          {authUser && authUser.id === course.User.id ? (
+            <Link to={`/courses/${id}/update`} className="button">
+              Update Course
+            </Link>
+          ) : null}
+          {authUser && authUser.id === course.User.id ? (
+            <button className="button" onClick={handleDelete}>
+              Delete Course
+            </button>
+          ) : null}
+          <Link to="/" className="button button-secondary">
+            Return to List
+          </Link>
         </div>
-        {courseDetail}
       </div>
-        : null
-  )
-}
+      {courseDetail}
+    </div>
+  ) : null;
+};
 
 export default CourseDetail;
